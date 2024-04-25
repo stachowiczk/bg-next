041fd95267121081a0a156dbf1001174fa3fcf8f
Karol Stachowicz <kstachowicz@icloud.com>
2024-04-25 14:27:07 +0200

    add @media queries for narrow screens

--- src/app/_components/contents/polityka-prywatnosci.ts
+++ src/app/_components/contents/polityka-prywatnosci.ts
@@ -1,6 +1,104 @@
-export const POLITYKA = `<h1>Polityka prywatności</h1>\n
-<hr>\n
-<p>Polityka prywatności serwisu internetowego</p>\n
-<p>1. Informacje ogólne</p>\n
-<p>Niniejsza Polityka Prywatności określa zasady przetwarzania i ochrony danych osobowych przekazanych przez Użytkowników w związku z korzystaniem przez nich usług serwisu internetowego dostępnego pod adresem URL: www.serwis.pl</p>\n
-<p>2. Administratorem danych osobowych zawartych w serwisie jest firma: Firma, adres, NIP, REGON</p>\n`
+export const POLITYKA = `
+Administratorem danych osobowych jest Beata Gruza prowadząca działalność pod firmą Kancelaria Adwokacka Adwokat Beata Gruza z siedzibą w Warszawie ul. Czerniowiecka nr 9 lok. 55 (02-705 Warszawa).
+
+<br/>
+<br/>
+<b>Lista procesów, w których administrator przetwarza dane osobowe:</b>
+
+<br/>
+<br/>
+
+<ol>
+    <li>Działania zmierzające do zawarcia i realizacji umowy z Klientem:
+        <ol type="a">
+            <li>Dane osobowe przetwarzane są na podstawie art. 6 ust. 1 lit. b RODO w zakresie Klientów będących osobami fizycznymi.</li>
+            <li>W przypadku osób reprezentujących Klientów, a także występujących w ich imieniu dane osobowe przetwarzane są na podstawie art. 6 ut. 1 lit. f RODO.</li>
+            <li>Dane osobowe będą przetwarzane przez okres trwania umowy oraz przez okres:
+                <ol type="i">
+                    <li>5 lat od końca roku, w którym zakończyło się postępowanie, w którym dane osobowe zostały zgromadzone – w przypadku danych osobowych przetwarzanych przez organy adwokatury oraz organy izb adwokackich w zakresie niezbędnym do prawidłowej realizacji zadań publicznych określonych w ustawie oraz danych osobowych przetwarzanych w ramach nadzoru nad adwokaturą.</li>
+                    <li>10 lat od końca roku, w którym zakończyło się postępowanie, w którym dane osobowe zostały zgromadzone – w przypadku danych osobowych przetwarzanych:
+                    <li>15 lat od końca roku, w którym zakończyło się postępowanie, w którym dane osobowe zostały zgromadzone – w przypadku danych osobowych przetwarzanych w toku prowadzonych przez organy adwokatury oraz organy izb adwokackich postępowań dyscyplinarnych wobec adwokatów i aplikantów adwokackich oraz podczas wykonywania kompetencji nadzorczych nad postępowaniami dyscyplinarnymi w sprawach adwokatów i aplikantów adwokackich.</li>
+                            a) w toku prowadzonych przez organy adwokatury oraz organy izb adwokackich postępowań:
+                                
+                                    - administracyjnych,
+                                    - w zakresie skarg i wniosków,
+                                    - innych przewidzianych przez ustawę lub wydane na podstawie ustawy akty prawne organów samorządu adwokackiego dotyczących adwokatów, aplikantów adwokackich lub osób ubiegających się o wpis na listę adwokatów lub listę aplikantów adwokackich, a także osób przystępujących do egzaminu wstępnego na aplikację adwokacką i egzaminu adwokackiego.
+                            
+                            b) w ramach nadzoru nad postępowaniami, o których mowa w lit. a.
+                            c) przez adwokatów w ramach wykonywania zawodu.
+                    </li>
+                </ol>
+            </li>
+        </ol>
+    </li>
+    <li>Działania zmierzające do zawarcia i realizacji umowy z Kontrahentem:
+        <ol type="a">
+            <li>Dane osobowe przetwarzane są na podstawie art. 6 ust. 1 lit. b RODO w zakresie Kontrahentów będących osobami fizycznymi.</li>
+            <li>W przypadku osób reprezentujących Kontrahentów, a także występujących w ich imieniu dane osobowe przetwarzane są na podstawie art. 6 ust. 1 lit. f RODO.</li>
+            <li>Dane w tym celu będą przetwarzane przez czas określony umową.</li>
+        </ol>
+    </li>
+    <li>Ustalenie, dochodzenie i obrona przed roszczeniami:
+        <ol type="a">
+            <li>Dane osobowe przetwarzane są na podstawie art. 6 ust. 1 lit. f RODO, który wskazuje na prawnie uzasadniony interes administratora danych w celu ustalenia, dochodzenia i obrony przed roszczeniami.</li>
+            <li>Dane w tym celu będą przetwarzane do czasu przedawnienia ewentualnych roszczeń prawnych.</li>
+        </ol>
+    </li>
+    <li>Realizacja obowiązków prawnych nałożonych na Administratora, dotyczących prowadzenia rachunkowości i dokumentacji księgowej, w tym archiwizowanie dokumentów:
+        <ol type="a">
+            <li>Dane osobowe przetwarzane są na podstawie art. 6 ust. 1 lit. c RODO.</li>
+            <li>Dane przetwarzane są przez okresy wskazane w przepisach prawa, w tym ustawę o rachunkowości.</li>
+            <li>Dokumenty księgowe przechowywane są co do zasady przez 5 lat po zakończeniu roku, w którym wystąpiło zdarzenie w związku, z którym został wystawiony dokument księgowy.</li>
+        </ol>
+    </li>
+    <li>Prowadzenie działań marketingowych z wykorzystaniem środków komunikacji elektronicznej:
+        <ol type="a">
+            <li>Dane osobowe przetwarzane są na podstawie art. 6 ust. 1 lit. f RODO, przy czym działania te ze względu na inne obowiązujące przepisy: Prawo telekomunikacyjne i Ustawę o świadczeniu usług drogą elektroniczną, prowadzone są jedynie na podstawie posiadanych zgód na kanał komunikacji.</li>
+            <li>Dane przetwarzane są do czasu wniesienia sprzeciwu lub wycofania zgody również przez aktywne działanie.</li>
+        </ol>
+    </li>
+
+
+</ol>
+
+<br/>
+<br/>
+<b>Odbiorcy danych osobowych:</b>
+Dane osobowe mogą być przekazywane następującym kategoriom podmiotów: (a) firmom świadczącym na rzecz administratora usługi: Informatyczne, doradztwa prawnego, kurierskie, pocztowe, (b) podmiotom publicznym na podstawie obowiązujących przepisów prawa.
+<br/>
+<br/>
+Przekazywanie danych osobowych do państwa trzecich lub organizacji międzynarodowych: 
+Dane osobowe nie są przekazywane do państwa trzeciego lub organizacji międzynarodowych. 
+
+<br/>
+<br/>
+<b>Prawa wynikające z RODO:</b>
+Przysługuje Państwu prawo dostępu do danych osobowych (art. 15 ust. 1 RODO), otrzymania kopii danych osobowych (art. 15 ust. 3 RODO), sprostowania lub uaktualnienia danych osobowych (art. 16 RODO), usunięcia danych osobowych (art. 17 RODO), ograniczenia przetwarzania danych osobowych (art. 18 RODO), przenoszenia swoich danych osobowych (art. 20 RODO). 
+
+<br/>
+<br/>
+Zgodnie z art. 16a ustawy z dnia 26 maja 1982 r. Prawo o adwokaturze przepisy art. 15 ust. 1 i 3, art. 18 i art. 19 RODO stosuje się w zakresie, w jakim nie naruszają obowiązku zachowania przez adwokata tajemnicy, o której mowa w art. 6 ww. ustawy.
+
+<br/>
+<br/>
+Mają Państwo także prawo wniesienia skargi do Prezesa Urzędu Ochrony Danych Osobowych (ul. Stawki 2, 00-193 Warszawa), gdy uznają Państwo, iż przetwarzanie Pani/Pana danych osobowych narusza przepisy RODO. 
+
+<br/>
+<br/>
+W celu skorzystania z przysługujących praw, należy kontaktować się z administratorem pod adresem: beata.gruza@adwokatura.pl lub listownie na adres siedziby administratora danych. 
+
+<br/>
+<br/>
+<b>Profilowanie: </b>
+Dane osobowe nie są wykorzystywane w celu zautomatyzowanego podejmowania decyzji, w tym profilowania. 
+
+<br/>
+<br/>
+<b>Informacja o obowiązku podania danych osobowych i skutkach ich niepodania: </b>
+Podanie danych osobowych jest dobrowolne w celu zawarcia i wykonania umowy oraz w celu realizacji prawnie uzasadnione interesu administratora. 
+
+<br/>
+<br/>
+W pozostałych przypadkach podanie danych jest obowiązkowe, w szczególności, gdy wymaga tego spełnienie obowiązku prawnego ciążącego na administratorze. 
+
+`
