0341c5305be09a2b8324ce297814cbd99678d5f1
Karol Stachowicz <kstachowicz@icloud.com>
2024-04-27 12:16:53 +0200

    fix

--- src/app/_components/wsp-text.tsx
+++ src/app/_components/wsp-text.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export function CollabText({ content }: Props) {
   return (
-    <div className={styles.container__blog} style={{marginTop: "20vh"}}>
+    <div className={styles.container__blog} >
       <div className={styles.blog__post} dangerouslySetInnerHTML={{ __html: content }}></div>
     </div>
   );
